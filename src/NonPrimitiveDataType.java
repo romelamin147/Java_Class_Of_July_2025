1f8010ec94839872c4d98b830a3af85ac8597b72
romelamin147 <romelamin147@gmail.com>
2025-09-03 21:45:20 -0400

    we have added a string called b

--- src/NonPrimitiveDataType.java
+++ src/NonPrimitiveDataType.java
@@ -3,5 +3,8 @@ public class NonPrimitiveDataType {
     public static void main(String[] args) {
         String a = "hello";
         System.out.println(a);
+
+        String b = "hi";
+        System.out.println(b);
     }
 }
